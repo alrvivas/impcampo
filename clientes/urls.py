0a212c3810fc4504edc5835d10188a51475a3fa3
alrvivas <alr.vivas@gmail.com>
2016-02-22 13:49:46 -0600

    v0.1

--- clientes/urls.py
+++ clientes/urls.py
@@ -4,6 +4,6 @@ urlpatterns = patterns('',
 	#url(r'^$', 'empleados.views.index', name='index'),
 	url(r'^$', 'clientes.views.clientes', name='clientes'),	
 	url(r'^perfil/(?P<cliente_id>[-\w]+)$', 'clientes.views.cliente', name='cliente'),
-	url(r'^add-cliente/(?P<cliente_id>[-\w]+)$', 'clientes.views.add_cliente', name='add-cliente'), 
+	url(r'^add-cliente$', 'clientes.views.add_cliente', name='add-cliente'), 
 	
 )
