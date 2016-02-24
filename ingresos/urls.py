cc8f50da18b422b318c5fe2654931741da3b4ce6
alrvivas <alr.vivas@gmail.com>
2016-02-24 15:42:14 -0600

    v0.1

--- ingresos/urls.py
+++ ingresos/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
 	#url(r'^$', 'empleados.views.index', name='index'),
 	url(r'^$', 'ingresos.views.ingresos', name='ingresos'),	
-	url(r'^ingreso/(?P<egreso_id>[-\w]+)$', 'egresos.views.ingreso', name='ingreso'),
+	url(r'^ingreso/(?P<egreso_id>[-\w]+)$', 'ingresos.views.ingreso', name='ingreso'),
 	url(r'^add-ingreso/$', 'ingresos.views.add_ingreso', name='add-ingreso'),
 	url(r'^editar-ingreso/(?P<egreso_id>[-\w]+)$', 'ingresos.views.edit_ingreso', name='editar-ingreso'),
 	
