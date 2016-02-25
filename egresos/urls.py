ca352633f8b7232e21749abaa1a7c37e5dcc8472
alrvivas <alr.vivas@gmail.com>
2016-02-24 18:50:37 -0600

    v0.1

--- egresos/urls.py
+++ egresos/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
 	url(r'^egreso/(?P<egreso_id>[-\w]+)$', 'egresos.views.egreso', name='egreso'),
 	url(r'^add-egreso/$', 'egresos.views.add_egreso', name='add-egreso'),
 	url(r'^editar-egresos/(?P<egreso_id>[-\w]+)$', 'egresos.views.edit_egreso', name='editar-egreso'),
+	url(r'^graficar/$', 'egresos.views.graficar_egreso', name='graficar-egreso'),
 	
 )
